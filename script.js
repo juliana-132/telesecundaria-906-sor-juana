09dbee76640c83c571856debfd7278429a66e54b
juliana-132 <jm2089458@gmail.com>
2025-06-22 22:04:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
